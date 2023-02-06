66007a3ffe36af39dac5f7674e3c8ff99d8d66a3
jasper-100 <102265966+jasper-100@users.noreply.github.com>
2023-02-05 20:43:39 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace helloWorld
         static void Main(string[] args)
         {
             
-                Console.WriteLine("Hello World");
+                Console.WriteLine("Helo World");
           
         }
     }
